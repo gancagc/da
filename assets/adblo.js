4e654872ee8f90ad9aac6fa448c397cf1ed150fe
Agc Lagz <94838289+agczlag@users.noreply.github.com>
2021-11-23 22:43:32 +1100

    Update adblo.js

--- assets/adblo.js
+++ assets/adblo.js
@@ -1,8 +1,8 @@
-function Home() { 
+function { 
   var div = document.getElementsByClassName("go-logo")[0].getElementsByTagName('a')[0].innerHTML="GO LAGU™";
 };
 
-function Dl()  {
-var a = document.getElementsByClassName("logo")[0].innerHTML="GO LAGU™";
-};
+function {
+document.getElementsByClassName("logo")[0].innerHTML="GO LAGU™";
+}
 
