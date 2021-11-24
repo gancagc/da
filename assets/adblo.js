c51df56d4f7ca043afe709b9f1a00185a5fef41f
Agc Lagz <94838289+agczlag@users.noreply.github.com>
2021-11-24 17:52:45 +1100

    Update adblo.js

--- assets/adblo.js
+++ assets/adblo.js
@@ -1,19 +1,8 @@
 var div = document.getElementsByClassName("go-logo")[0].getElementsByTagName('a')[0].innerHTML="GO LAGU™";
-let dbAds=[
-  {
-    "target-selector":[
-      ".container",
-      "#container",
-      ".content",
-      ".pa15.bgwhite"
-    ],
-    "position":"out-bottom", //out-top, out-bottom, in-top, in-bottom
-    "data" :`<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-    <!-- Iklan Header -->
-<div class="id_164">
-<a href="https://download.imglov.com/gc7ukfi8t?key=ef1ec2dfe94947911612309890f9573b" rel="nofollow" target="_blank">
-<div class="to_707">DOWNLOAD MP3</div>
-</a>
-</div>    `
-  }
-];
+var variable = "" + 
+"<div class=\"id_164\">" + 
+"<a href=\"https://download.imglov.com/gc7ukfi8t?key=ef1ec2dfe94947911612309890f9573b\" rel=\"nofollow\" target=\"_blank\">" + 
+"<div class=\"to_707\">DOWNLOAD MP3</div>" + 
+"</a>" + 
+"</div>" + 
+"";
