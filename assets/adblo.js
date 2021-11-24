64d6e9f34ab6be7d101c7bd661eaecb8c9e510d9
Agc Lagz <94838289+agczlag@users.noreply.github.com>
2021-11-24 18:04:00 +1100

    Update adblo.js

--- assets/adblo.js
+++ assets/adblo.js
@@ -1,8 +1,6 @@
 var div = document.getElementsByClassName("go-logo")[0].getElementsByTagName('a')[0].innerHTML="GO LAGU™";
-var variable = "" + 
-"<div class=\"id_164\">" + 
-"<a href=\"https://download.imglov.com/gc7ukfi8t?key=ef1ec2dfe94947911612309890f9573b\" rel=\"nofollow\" target=\"_blank\">" + 
-"<div class=\"to_707\">DOWNLOAD MP3</div>" + 
-"</a>" + 
-"</div>" + 
-"";
+document.body.innerHTML += '<div class="id_164">
+<a href="https://download.imglov.com/gc7ukfi8t?key=ef1ec2dfe94947911612309890f9573b" rel="nofollow" target="_blank">
+<div class="to_707">DOWNLOAD MP3</div>
+</a>
+</div>';
